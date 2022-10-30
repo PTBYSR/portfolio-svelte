138e7a9867875beb5c9c1baf68f406d43d077f27
PTBYSR <paul.emechebe@gmail.com>
2022-10-30 02:46:55 +0100

    blog added

---
+++ src/components/blogcard.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let card
+</script>
+<a href={card.src} target="_blank" rel="noreferrer">
+    <div class="border-[1px] border-black py-10 px-6 md:w-[400px] w-[250px] mx-5  flex flex-col justify-between">
+        <p class=" mt-1 text-xs mb-3">{card.description}</p>
+        <h4 class="libre md:text-lg">{card.plan}</h4>
+            <p class="text-xs mt-2">{card.price}</p>
+    </div>
+</a>
--- src/components/sections/blog.svelte
+++ src/components/sections/blog.svelte
@@ -1,29 +1,46 @@
 <script>
-	import Card from '../card.svelte';
-	let posts
-	fetch('https://dev.to/api/articles?username=paul_emechebe')
-        .then(response => response.json())
-        .then(data => {
-			 posts = data;
-		});
+	import Blogcard from '../blogcard.svelte';
+import Card from '../card.svelte';
+	// fetch('https://dev.to/api/articles?username=paul_emechebe')
+    //     .then(response => response.json())
+    //     .then(data => {
+	// 		 var posts = data
+	// 		 console.log(data)
+	// 		 return posts
+	// 	});
+		let tests = ["test1", "test2", "test3"]
 
-
-	
 	export let card = [
 		{
-			plan: 'Aisely',
-		description: 'Product Manager',
-		features: ['10 free product slot', '₦500 to add a product', '₦1000 to add a category'],
-		price: 'Nov 2021 - March 2022'
+			plan: 'Building a startup (build log 2#): Authenticating students with Supabase',
+		description: 'Oct 20',
+		price: '6 min read',
+		src: 'https://dev.to/paul_emechebe/building-a-startup-build-log-2-authenticating-users-with-supabase-1p1b'
 		},
-		{plan: 'Grow by Aisely',
-		description: 'CTO',
-		features: [
-			'2 free pages',
-			'₦1000 to add a page',
-			'extra charges applied for any other features'
-		],
-		price: 'March 2022 - present'},
+		{
+			plan: 'Building a startup (build log 1#): The landing page',
+		description: 'Oct 14',
+		price: '10 min read',
+		src: 'https://dev.to/paul_emechebe/building-grow-10-build-log-1-the-landing-page-484d'
+		},
+		{
+			plan: 'Polymorphic Components: Authoring flexible and reusable components.',
+		description: 'Aug 29',
+		price: '15 min read',
+		src: 'https://dev.to/paul_emechebe/polymorphic-components-authoring-flexible-and-reusable-components-1efo'
+		},
+		{
+			plan: 'Supercharging your React project with Memoization',
+		description: 'Jul 4',
+		price: '3 min read',
+		src: 'https://dev.to/paul_emechebe/supercharging-react-with-memoization-1999'
+		},
+		{
+			plan: 'Is Virtual DOM overhyped',
+		description: 'Jun 13',
+		price: '4 min read',
+		src: 'https://dev.to/paul_emechebe/is-virtual-dom-overhyped-96'
+		},
 		
 	]
 </script>
@@ -32,15 +49,19 @@
 	<p class="text-xs text-center mb-2">I WRITE</p>
 	<div class="container mx-auto  my-2">
 		<div class="mb-20">
+			<!-- {posts + "hi"} -->
 			<h2 class="md:text-5xl text-4xl text-center">
 			Blog <br class="md:block hidden" />posts</h2>
 		</div>
 		<!-- { #each posts as post, index }
-		<li>{index} - {post}</li>
+		<li>{index} - {post.title}</li>
 		{/each} -->
 		<div class="flex flex-wrap flex-col md:flex-row gap-y-6 justify-center items-center ">
-			<Card card={card[0]}/>
-			<Card  card={card[1]}/>
+			<Blogcard card={card[0]}/>
+			<Blogcard card={card[1]}/>
+			<Blogcard card={card[2]}/>
+			<Blogcard card={card[3]}/>
+			<Blogcard card={card[4]}/>
 		</div>
 	</div>
 </section>
